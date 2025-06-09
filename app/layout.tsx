4e0d98f17223bd7f062ce3874bfcf607c7bb3a2d
Reuben Ellis <ethriel3695@gmail.com>
2025-06-09 16:39:38 -0600

    Add meticulous

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,17 @@ export default async function Layout({
 
   return (
     <html lang="en">
+      <head>
+        {(process.env.NODE_ENV === 'development' ||
+          process.env.VERCEL_ENV === 'preview') && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-recording-token="Pj5FKQCNkHQTAcwznr4kOkI3blWjMPZdHEP27xfj"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </head>
       <body
         className={cn('flex min-h-svh flex-col antialiased', inter.className)}
       >
